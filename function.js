84198ea8726aad31c54ff6f86c1933dae72a7415
Sharan112233 <138100505+Sharan112233@users.noreply.github.com>
2024-09-29 22:59:18 +0530

    added function.js

---
+++ function.js
@@ -0,0 +1,44 @@
+
+// // let addition=(a,b)=>{
+// //     return a+b;
+// // }
+// // console.log(addition(12.85,22))
+
+// // let sub=(a,b)=>{
+// //     return a-b;
+// // }
+// // console.log(sub(12.85,22))
+
+// // let multi=(a,b)=>{
+// //     return a*b;
+// // }
+// // console.log(multi(12.85,22))
+
+// // let divison=(a,b)=>{
+// //     return a/b;
+// // }
+// // console.log(divison(12.85,22))
+
+// // let modules=(a,b)=>{
+// //     return a%b;
+// // }
+// // console.log(modules(12.85,22))
+// studnets=['pavan','pavan','pavan','pavan','pavan','pavan','pavan']
+// studnets.forEach( element => {
+//     console.log(element)
+// });
+// std={
+//     name:"sharan",
+//     usn:34,
+//     class:"b",
+//     marks:85
+// }
+// for (const key in std) {
+
+//         console.log(`${std[key]}`)
+//     }
+let min=1;
+let max=10;
+
+console.log(Math.random()*(max-min+1)+min);
+console.log(((max-min+1))+min)
